332b326113a7ecac6fc2b2970347f0b546bb0ac0
Maurice <maurice.molin@gmail.com>
2020-11-02 12:32:35 -0500

    Initial project commit.

---
+++ src/main/java/tost/springframework/recipe/mmrecipe/MmRecipeApplication.java
@@ -0,0 +1,13 @@
+package tost.springframework.recipe.mmrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tost/springframework/recipe/mmrecipe/MmRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package tost.springframework.recipe.mmrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
